cf570089cd839347e886f9df8c3f7ec7eb4f4292
Tim Batty <tbatty7@gmail.com>
2025-10-28 16:39:35 -0400

    Moved testable code to Core and generated test

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -1,9 +1,13 @@
-﻿namespace MyApp.Tests;
+﻿using FluentAssertions;
+
+namespace MyApp.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void canAdd()
     {
+        var result = 1 + 1;
+        result.Should().Be(2);
     }
 }
---
+++ MyApp.Tests/WeatherForecastTest.cs
@@ -0,0 +1,25 @@
+using FluentAssertions;
+
+namespace MyApp.Tests;
+
+public class WeatherForecastTest
+{
+
+    [Fact]
+    public void canSeeWeather()
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering",
+            "Scorching"
+        };
+        DateOnly date = DateOnly.FromDateTime(DateTime.Now.AddDays(1));
+        WeatherForecast result = new WeatherForecast
+        (
+            date,
+            28,
+            summaries[2]
+        );
+        result.Should().Be(new WeatherForecast(date, 28, "Chilly"));
+    }
+}
